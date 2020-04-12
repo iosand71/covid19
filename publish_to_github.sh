48d07cdb9e6d3b427d2c1cadda64f0b8122df4a1
Andrea Iosio <aiosio@gmail.com>
2020-04-12 18:11:29 +0200

    update and change export to github

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 jupyter nbconvert --template=nbextensions --to=html covid19-italia.ipynb
-cp covid19-italia.html ~/Projects/iosand/public/
-cd ~/Projects/iosand/
+cp covid19-italia.html ~/Projects/iosand71.github.io/
+cd ~/Projects/iosand71.github.io
 git add .
 git commit -m "update covid report"
-git push heroku master
+git push origin master
