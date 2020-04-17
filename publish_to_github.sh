024476b76ce19672d937051fa5d5e6a4b513e74f
Andrea Iosio <andrea.iosio@rgigroup.com>
2020-04-17 18:42:04 +0200

    update covid report

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 jupyter nbconvert --template=nbextensions --to=html covid19-italia.ipynb
-git commit --amend --no-edit
 cp covid19-italia.html ~/Projects/iosand71.github.io/
 cd ~/Projects/iosand71.github.io
 git add .
