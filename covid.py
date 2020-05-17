37020f1e82ce20ad98dd313907d3397e978c01b6
Andrea Iosio <aiosio@gmail.com>
2020-05-17 17:22:35 +0200

    minor refactor

--- covid.py
+++ covid.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 # coding: utf-8
+""" Covid 
+
+Covid19 data analysis for italy
+
+"""
 import locale
 import sys, getopt
 import pandas as pd
 import numpy as np
 from datetime import datetime,timedelta
-# Config
 
 url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
-
-# Implementation
-
 data = pd.read_csv(url)
 data.data = pd.to_datetime(data.data)
 data['nuovi_decessi'] = data.deceduti.diff()
@@ -18,16 +19,110 @@ data['mortalita'] = data.deceduti / data.totale_casi
 data['guariti'] = data.dimessi_guariti / data.totale_casi
 data['ricoverati'] = data.totale_ospedalizzati / data.totale_casi
 data['intensivi'] = data.terapia_intensiva / data.totale_casi
+
 data_pct = data[data.columns.difference(['data','stato','note_it','note_en'])].pct_change()
+data_pct.insert(0,'data', data.data)
+
+statistics = data.loc[:,['data','totale_casi','totale_positivi','nuovi_positivi','variazione_totale_positivi','deceduti',
+                       'nuovi_decessi','terapia_intensiva','totale_ospedalizzati','dimessi_guariti',
+                       'tamponi','casi_testati']]
+statistics.insert(0,'% mortalita', data.mortalita)
+statistics.insert(0,'% intensivi', data.intensivi)
+statistics.insert(0,'% ricoverati', data.ricoverati)
+statistics.insert(0,'% guariti', data.guariti)
+
+recap = pd.DataFrame({
+    'Valore assoluto': [data.totale_casi.iloc[-1],data.totale_positivi.iloc[-1], data.nuovi_positivi.iloc[-1],
+                        data.variazione_totale_positivi.iloc[-1], data.deceduti.iloc[-1],
+                        data.nuovi_decessi.iloc[-1], data.terapia_intensiva.iloc[-1],
+                        data.totale_ospedalizzati.iloc[-1], data.dimessi_guariti.iloc[-1],
+                        data.tamponi.iloc[-1],data.casi_testati.iloc[-1], data.mortalita.iloc[-1], data.intensivi.iloc[-1],
+                        data.ricoverati.iloc[-1], data.guariti.iloc[-1]],
+    'Variazione tot rispetto a ieri': [data.totale_casi.diff().iloc[-1],data.totale_positivi.diff().iloc[-1], data.nuovi_positivi.diff().iloc[-1],
+                                     data.variazione_totale_positivi.diff().iloc[-1], data.deceduti.diff().iloc[-1],
+                                     data.nuovi_decessi.diff().iloc[-1], data.terapia_intensiva.diff().iloc[-1],
+                                     data.totale_ospedalizzati.diff().iloc[-1], data.dimessi_guariti.diff().iloc[-1],
+                                     data.tamponi.diff().iloc[-1],data.casi_testati.diff().iloc[-1], data.mortalita.diff().iloc[-1],
+                                     data.intensivi.diff().iloc[-1], data.ricoverati.diff().iloc[-1],
+                                     data.guariti.diff().iloc[-1]],
+    'Variazione % rispetto a ieri': [data_pct.totale_casi.iloc[-1],data_pct.totale_positivi.iloc[-1], data_pct.nuovi_positivi.iloc[-1],
+                                     data_pct.variazione_totale_positivi.iloc[-1], data_pct.deceduti.iloc[-1],
+                                     data_pct.nuovi_decessi.iloc[-1], data_pct.terapia_intensiva.iloc[-1],
+                                     data_pct.totale_ospedalizzati.iloc[-1], data_pct.dimessi_guariti.iloc[-1],
+                                     data_pct.tamponi.iloc[-1], data_pct.casi_testati.iloc[-1], data_pct.mortalita.iloc[-1],
+                                     data_pct.intensivi.iloc[-1], data_pct.ricoverati.iloc[-1],
+                                     data_pct.guariti.iloc[-1]]
+
+}, index=['Totale casi','Attualmente positivi','Totale nuovi casi', 'Variazione nuovi casi', 'Totale decessi',
+             'Variazione decessi','Terapia intensiva','Ospedalizzati','Dimessi','Totale tamponi','Casi testati',
+             'Mortalità', 'Critici', 'Ricoverati','Guariti'])
+
+pivot = pd.pivot_table(statistics, columns=['data'])
+pivot.sort_values(axis = 1, by=['data'], ascending=False, inplace=True)
+pivot.columns = pivot.columns.strftime('%d/%m/%Y')
+
 updated_at = data.data.iloc[-1].strftime('%d/%m/%Y')
-locale.setlocale(locale.LC_ALL, 'it_IT.utf-8')
+
+def thousands(value):
+  return f'{value:,}'
+
+class DailyStats:
+    """ Daily stats for covid19 in Italy """
+
+    def __init__(self):
+      pass
+
+    def print_last_day(label, column):
+      last = data[column].iloc[-1]
+      pct_var = data_pct[column].iloc[-1]
+      tabs = '\t' if last >= 100000 else '\t\t'
+      print(label, thousands(last), end='\t')
+      print(f' {tabs}( {pct_var:+.2%} )')
+
+    def print_as_pct(label, column):
+      pct = data[column].iloc[-1]
+      pct_pct = data_pct[column].iloc[-1]
+      tabs = '\t\t\t' if pct <= 0.1 else '\t\t'
+      print(label, f'{pct:.2%}', end='')
+      print(f' {tabs}( {pct_pct:+.2%} )')
+    
+    def dictionary():
+      for name in data.columns:
+        print(name)
+
+    def print():
+      # Data model:
+      # ricoverati_con_sintomi terapia_intensiva totale_ospedalizzati 
+      # isolamento_domiciliare totale_positivi variazione_totale_positivi 
+      # nuovi_positivi dimessi_guariti deceduti totale_casi tamponi casi_testati 
+      # note_it note_en
+
+      print("Aggiornamento: ", updated_at, "\t\tvariazione rispetto a ieri")
+      print("------------------------------------------------------------------")
+      DailyStats.print_last_day("Totale casi: \t\t", 'totale_casi')
+      DailyStats.print_last_day("Totale nuovi casi: \t", 'nuovi_positivi')
+      DailyStats.print_last_day("Variazione nuovi casi: \t", 'variazione_totale_positivi')
+      DailyStats.print_last_day("Totale positivi: \t", 'totale_positivi')
+      DailyStats.print_last_day("Totale decessi: \t", 'deceduti')
+      DailyStats.print_last_day("Variazione decessi: \t", 'nuovi_decessi')
+      DailyStats.print_last_day("Terapia intensiva: \t", 'terapia_intensiva')
+      DailyStats.print_last_day("Ospedalizzati: \t\t", 'totale_ospedalizzati')
+      DailyStats.print_last_day("Dimessi: \t\t", 'dimessi_guariti')
+      DailyStats.print_last_day("Totale tamponi: \t", 'tamponi')
+      DailyStats.print_last_day("Totale testati: \t", 'casi_testati')
+      print()
+
+      DailyStats.print_as_pct("Mortalità: \t\t", 'mortalita')
+      DailyStats.print_as_pct("Critici: \t\t", 'intensivi')
+      DailyStats.print_as_pct("Ricoverati: \t\t", 'ricoverati')
+      DailyStats.print_as_pct("Guariti: \t\t", 'guariti')
+      print()
 
 def main(argv):
-  global log_scale
-  global days_range
   help_str = 'usage: covid.py'
+  locale.setlocale(locale.LC_ALL, 'it_IT.utf-8')
   try:
-    opts, args = getopt.getopt(argv, "hld:", ["logmode", "days="])
+    opts, args = getopt.getopt(argv, "h:")
   except getopt.GetoptError:
       print (help_str)
       sys.exit(2)
@@ -35,56 +130,9 @@ def main(argv):
     if opt == '-h':
       print (help_str)
       sys.exit()
+  DailyStats.print()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
 
-def thousands(value):
-  return f'{value:,}'
-
-def print_last_result(label, column):
-  global data
-  global data_pct
-  last = data[column].iloc[-1]
-  pct_var = data_pct[column].iloc[-1]
-  tabs = '\t' if last >= 100000 else '\t\t'
-  print(label, thousands(last), end='\t')
-  print(f' {tabs}( {pct_var:+.2%} )')
-
-def print_pct(label, column):
-  pct = data[column].iloc[-1]
-  pct_pct = data_pct[column].iloc[-1]
-  tabs = '\t\t\t' if pct <= 0.1 else '\t\t'
-  print(label, f'{pct:.2%}', end='')
-  print(f' {tabs}( {pct_pct:+.2%} )')
-
-def start():
-  global data
-  # Data model:
-  #   'data', 'stato', 'ricoverati_con_sintomi', 'terapia_intensiva',
-  #   'totale_ospedalizzati', 'isolamento_domiciliare', 'totale_positivi',
-  #   'variazione_totale_positivi', 'nuovi_positivi', 'dimessi_guariti',
-  #   'deceduti', 'totale_casi', 'tamponi', 'note_it', 'note_en'
-
-  print("Aggiornamento: ", updated_at, "\t\tvariazione rispetto a ieri")
-  print("------------------------------------------------------------------")
-  print_last_result("Totale casi: \t\t", 'totale_casi')
-  print_last_result("Totale nuovi casi: \t", 'nuovi_positivi')
-  print_last_result("Variazione nuovi casi: \t", 'variazione_totale_positivi')
-  print_last_result("Totale positivi: \t", 'totale_positivi')
-  print_last_result("Totale decessi: \t", 'deceduti')
-  print_last_result("Variazione decessi: \t", 'nuovi_decessi')
-  print_last_result("Terapia intensiva: \t", 'terapia_intensiva')
-  print_last_result("Ospedalizzati: \t\t", 'totale_ospedalizzati')
-  print_last_result("Dimessi: \t\t", 'dimessi_guariti')
-  print_last_result("Totale tamponi: \t", 'tamponi')
-  print_last_result("Totale testati: \t", 'casi_testati')
-  print()
-  print_pct("Mortalità: \t\t", 'mortalita')
-  print_pct("Critici: \t\t", 'intensivi')
-  print_pct("Ricoverati: \t\t", 'ricoverati')
-  print_pct("Guariti: \t\t", 'guariti')
-
-  print()
-
-start()
+# vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
