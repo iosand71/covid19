9cd40df6de114bba7a14ceb4a9bd1a38eae58e59
Andrea Iosio <andrea.iosio@rgigroup.com>
2020-08-19 17:55:00 +0200

    today updates

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -6,7 +6,7 @@ jupyter nbconvert --template=nbextensions --to=html covid19-lombardia.ipynb
 cp covid19-*.html ../iosand71.github.io/
 cd ../iosand71.github.io
 git add .
-git commit -m "update covid report"
+git commit -m "update covid reports"
 git push origin master
 cd ../covid19
 rm covid19-*.html
