99c904a655ae92ffcc5e93012f3150a16e360da5
Andrea Iosio <andrea.iosio@rgigroup.com>
2020-08-21 17:15:21 +0200

    add international report

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # jupyter nbconvert --ExecutePreprocessor.store_widget_state=True --to notebook --execute covid19-italia.ipynb
 jupyter nbconvert --template=nbextensions --to=plotlyhtml covid19-italia.ipynb
-jupyter nbconvert --template=nbextensions --to=html covid19-piemonte.ipynb
-jupyter nbconvert --template=nbextensions --to=html covid19-lombardia.ipynb
+jupyter nbconvert --template=nbextensions --to=plotlyhtml covid19-piemonte.ipynb
+jupyter nbconvert --template=nbextensions --to=plotlyhtml covid19-lombardia.ipynb
+jupyter nbconvert --template=nbextensions --to=plotlyhtml covid19-internazionale.ipynb
 cp covid19-*.html ../iosand71.github.io/
 cd ../iosand71.github.io
 git add .
